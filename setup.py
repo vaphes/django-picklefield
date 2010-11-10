23fbafc2825cbc89063625a3b96bdcf9bda52f72
Gintautas Miliauskas <gintautas@miliauskas.lt>
2010-11-10 09:01:13 +0200

    Updated changelog, bumped version to 0.1.9.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@
 from setuptools import setup, find_packages
 
 setup(name='django-picklefield',
-    version='0.1.8',
+    version='0.1.9',
     description='Pickled object field for Django',
     long_description=open('README').read(),
     author='Gintautas Miliauskas',
