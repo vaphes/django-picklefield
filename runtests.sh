857ff99db2fddb0475d6503ccf5afaddc96390e7
Gintautas Miliauskas <gintautas@miliauskas.lt>
2013-11-15 22:26:46 +0100

    Updated runtests script.

--- runtests.sh
+++ runtests.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-DJANGO_SETTINGS_MODULE=test_settings django-admin.py test --pythonpath `pwd` --settings test_settings picklefield
+ln -s src/picklefield .
+DJANGO_SETTINGS_MODULE=test_settings django-admin test --pythonpath `pwd` --settings test_settings picklefield
+rm -f picklefield
