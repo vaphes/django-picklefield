9f8b2cc85fbd75cf9194b99dbef8856b3f1278ee
Simon Charette <charette.s@gmail.com>
2015-09-03 16:05:49 -0400

    Avoid using the deprecated _get_val_from_obj method.

--- src/picklefield/fields.py
+++ src/picklefield/fields.py
@@ -152,7 +152,7 @@ class PickledObjectField(_PickledObjectField):
         return value
 
     def value_to_string(self, obj):
-        value = self._get_val_from_obj(obj)
+        value = self.value_from_object(obj)
         return self.get_db_prep_value(value)
 
     def get_internal_type(self):
