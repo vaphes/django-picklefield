f5bb1278f27e46be251cdf4f55d07e5a3b0f134a
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2017-05-06 15:05:34 +0200

    Removed Python 2.6 from setup.py.

--- setup.py
+++ setup.py
@@ -40,7 +40,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
