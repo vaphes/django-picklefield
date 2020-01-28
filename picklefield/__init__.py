2405a2afcd4cd2161e1393eaae525779ff6801b3
Simon Charette <charette.s@gmail.com>
2020-01-28 10:01:12 -0500

    Release 2.1.1 to account for packaging issues.

--- picklefield/__init__.py
+++ picklefield/__init__.py
@@ -7,6 +7,6 @@ from .fields import PickledObjectField
 
 __all__ = 'VERSION', '__version__', 'DEFAULT_PROTOCOL', 'PickledObjectField'
 
-VERSION = (2, 1, 0, 'final', 0)
+VERSION = (2, 1, 1, 'final', 0)
 
 __version__ = django.utils.version.get_version(VERSION)
